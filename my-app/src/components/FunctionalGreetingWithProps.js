1967ff814147160f38134712082cb84463a479c8
Alexa Hendry <alexahendry88@gmail.com>
2023-01-30 13:45:50 +0000

    Add props to functional component

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+
+// vanilla JS
 // function FunctionalGreeting() {
 //     return <h1>Hello from React!</h1>
 // }
 
-const FunctionalGreetingWithProps = () => <h1>Hello</h1>;
+// arrow function version
+// const FunctionalGreetingWithProps = () => <h1>Hello</h1>;
+
+// with props
+const FunctionalGreetingWithProps = (props) => {
+    // can console log out and view in DevTools console
+    // console.log(props);
+    return <h1>Hello, {props.name}! I see you're {props.age} years old; {props.greeting}!</h1>;
+}
 
 export default FunctionalGreetingWithProps;
