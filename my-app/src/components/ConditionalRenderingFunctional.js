acba2af80043ccd515a2ce3c6e2b6ef213d6da4a
Alexa Hendry <alexahendry88@gmail.com>
2023-02-01 16:33:40 +0000

    Add emmet extension on js files

--- my-app/src/components/ConditionalRenderingFunctional.js
+++ my-app/src/components/ConditionalRenderingFunctional.js
@@ -3,8 +3,7 @@ import React from 'react'
 function ConditionalRenderingFunctional(props) {
     return (
         <div>
-            <h1>{props.connected ? "Connected" : "Not Connected"}</h1>
-            
+            <h1>{props.connected ? "Connected" : "Not Connected"}</h1>        
         </div>
     )
 }
